dbc8691dc12fede6e56a9b327f8b1e9df486ec99
Uri <urifallon.sc.9125268@gmail.com>
2024-09-29 08:02:35 +0700

    create & update : connected database mysql

--- ProjectBase/ProjectBase/settings.py
+++ ProjectBase/ProjectBase/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'ProjectBase.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "home",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+
     }
 }
+
 
 
 # Password validation
